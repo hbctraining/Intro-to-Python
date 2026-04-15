118a604cabc8bf9af9f31e698527c0c59e888e9c
nsohail19 <20noor.sohail@gmail.com>
2026-04-15 14:31:13 -0400

    Download script for data folder

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-mkdir -p lessons/data
 mkdir -p lessons/results
+mkdir -p lessons/figures
 
 # Download Data
+curl -L "https://www.dropbox.com/scl/fi/gfuo9ywg1hmz5ozuh5ga8/data.zip?rlkey=8tpxtcmvgpi2xwweql8bnnrbu&dl=1" -o lessons/data.zip
+unzip lessons/data.zip -d lessons
+rm lessons/data.zip
+rm -rf lessons/__MACOSX
